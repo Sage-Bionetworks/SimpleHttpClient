32341080eeda0d5d36f97e0232f68c4e11dadd67
Kimyen <kimyen0816@gmail.com>
2016-11-17 14:31:26 -0800

    custom HttpClient with given timeout

--- src/main/java/org/sagebionetworks/simpleHttpClient/SimpleHttpClientImpl.java
+++ src/main/java/org/sagebionetworks/simpleHttpClient/SimpleHttpClientImpl.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
@@ -14,9 +15,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.config.SocketConfig;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
@@ -30,6 +33,19 @@ public class SimpleHttpClientImpl implements SimpleHttpClient{
 	 */
 	public SimpleHttpClientImpl() {
 		httpClient = HttpClients.createDefault();
+	}
+
+	/**
+	 * Create a custom SimpleHttpClient with the given connectionTimeOut and socketTimeOut
+	 * 
+	 * @param connectionTimeOutMs
+	 * @param socketTimeOutMs
+	 */
+	public SimpleHttpClientImpl(int connectionTimeOutMs, int socketTimeOutMs) {
+		HttpClientBuilder builder = HttpClientBuilder.create();
+		builder.setConnectionTimeToLive(connectionTimeOutMs, TimeUnit.MILLISECONDS);
+		builder.setDefaultSocketConfig(SocketConfig.custom().setSoTimeout(socketTimeOutMs).build());
+		httpClient = builder.build();
 	}
 
 	@Override
